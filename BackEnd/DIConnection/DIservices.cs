0130489d7bb5f31654c5c8a47b0f7fb6e54ad96d
GiaIn <yen.bg@viettinlab.com>
2022-11-22 23:07:44 +0700

    code new

---
+++ BackEnd/DIConnection/DIservices.cs
@@ -0,0 +1,20 @@
+﻿using BackEnd.Data.Command;
+using BackEnd.interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.DIConnection
+{
+    public static class DIservices
+    {
+        public static IServiceCollection ConfigDI(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IThanhVien, ThanhVienCommand>();
+            serviceCollection.AddSingleton<INoiQui, NoiQuiCommand>();
+            return serviceCollection;
+        }
+    }
+}
---
+++ BackEnd/Data/Command/NoiQuiCommand.cs
@@ -0,0 +1,28 @@
+﻿using BackEnd.Data.SQLConnection;
+using BackEnd.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Data.Command
+{
+    public class NoiQuiCommand : interfaces.INoiQui
+    {
+        private Dictionary<string, object> _dic = null;
+        public async Task<SQLObject.APIresult> GetNoiQuiasync(NoiQui NoiQui)
+        {
+            string SQL = "select * from NoiQuy";
+            return await SQLConnection.SQLConnection.Connection().SQLQuerryAsync(SQL);
+        }
+
+        public async Task<SQLObject.APIresult> UpdateNoiQuiasync(NoiQui NoiQui)
+        {
+            _dic = new Dictionary<string, object>();
+            string SQL = "update  NoiQuy set NoiDung=@NoiDung , NgayBanHanh=@NgayBanHanh";
+            _dic.Add("NoiDung", NoiQui.NoiDung);
+            _dic.Add("NgayBanHanh", NoiQui.NgayBanHanh);
+            return await SQLConnection.SQLConnection.Connection().ExcuteQuerryAsync(SQL, _dic);
+        }
+    }
+}
